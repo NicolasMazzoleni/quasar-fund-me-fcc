70484078ac6411807f77b7f23a8dfd96bd42cf78
Nicolas Mazzoleni <nicolasmazzoleni@Nicolass-MacBook-Pro.local>
2023-01-25 10:43:35 +0100

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn
+        @click="connect"
+        color="white"
+        text-color="black"
+        label="Connect"
+      />
+      <q-btn @click="getBalance" color="primary" label="getBalance" />
+      <q-btn @click="withdraw" color="secondary" label="Withraw" />
+      <q-form @click="onSubmit" class="q-gutter-md">
+        <q-input filled type="number" v-model="amount" label="Montant" />
+
+        <div>
+          <q-btn label="Fund" type="submit" color="primary" />
+        </div>
+      </q-form>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup() {
+    const amount = ref(null);
+
+    return {
+      amount,
+
+      onSubmit() {
+        console.log('yo', amount.value);
+      },
+
+      withdraw() {
+        console.log('withdraw');
+      },
+
+      connect() {
+        console.log('connect');
+      },
+
+      getBalance() {
+        console.log('get Balance');
+      },
+    };
+  },
+});
+</script>
