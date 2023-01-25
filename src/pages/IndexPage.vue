97e2e433f5136730127d8b0e554f7cdb5cc5b6ec
NICOLAS MAZZOLENI <mazzoleni.nicolas@gmail.com>
2023-01-25 12:13:38 +0100

    Adding inner-loading-spinner

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,9 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <div class="q-pa-md q-gutter-sm">
-      <q-btn
-        @click="connect"
-        color="white"
-        text-color="black"
-        label="Connect"
-      />
-      <q-btn @click="getBalance" color="primary" label="getBalance" />
-      <q-btn @click="withdraw" color="secondary" label="Withraw" />
+      <q-btn @click="connect" color="secondary" label="Connect" />
+      <q-btn @click="getBalance" color="accent" label="getBalance" />
+      <q-btn @click="withdraw" color="negative" label="Withraw" />
       <br /><br />
       <q-form @click="onSubmit" class="q-gutter-md">
         <q-input filled type="number" v-model="amount" label="Montant" />
@@ -17,6 +12,9 @@
           <q-btn label="Fund" type="submit" color="primary" />
         </div>
       </q-form>
+      <q-inner-loading :showing="loader">
+        <q-spinner-gears size="50px" color="orange-8" />
+      </q-inner-loading>
     </div>
   </q-page>
 </template>
@@ -28,9 +26,11 @@ export default defineComponent({
   name: 'IndexPage',
   setup() {
     const amount = ref(null);
+    const loader = ref(false);
 
     return {
       amount,
+      loader,
 
       onSubmit() {
         console.log('yo', amount.value);
@@ -40,8 +40,22 @@ export default defineComponent({
         console.log('withdraw');
       },
 
-      connect() {
-        console.log('connect');
+      async connect() {
+        const web3js = window.ethereum;
+        if (web3js !== undefined) {
+          try {
+            loader.value = true;
+            const response = await web3js.request({
+              method: 'eth_requestAccounts',
+            });
+
+            if (response.length) {
+              loader.value = false;
+            }
+          } catch (e) {
+            loader.value = false;
+          }
+        }
       },
 
       getBalance() {
